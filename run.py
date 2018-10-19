84667a5d60d64c40666e3d19a9f35db263abef3d
bryan-munene <bryanmuthuri38@gmail.com>
2018-10-20 00:54:56 +0300

    [Chore #161357745] formatted the code to fit pep8 style standards

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-from app import create_app # import the flask app from the function that creates it.
+# import the flask app from the function that creates it.
+from app import create_app
 
-CONFIG_TYPE = "development" # set the necessary config enviroment from the config.py file
+# set the necessary config enviroment from the config.py file
+CONFIG_TYPE = "development"
 app = create_app(CONFIG_TYPE)
 
-if __name__ == '__main__': # run the flask app
+if __name__ == '__main__':  # run the flask app
     app.run()

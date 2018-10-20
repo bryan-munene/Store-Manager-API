0b8c87e1de4861c62872e1bb0232bbeae49baba6
bryan-munene <bryanmuthuri38@gmail.com>
2018-10-20 16:18:14 +0300

    [Feature #161361606] set v1 as a version using blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -13,14 +13,16 @@ def create_app(config):
     app.url_map.strict_slashes = False
     app.config.from_object(app_config[config])
     app.config["TESTING"] = True
-
+    
     from app.api.v1.views.sales import sales_bp
     app.register_blueprint(sales_bp)
     from app.api.v1.views.items import items_bp
     app.register_blueprint(items_bp)
     from app.api.v1.views.auth import users_bp
     app.register_blueprint(users_bp)
-    from app.api.v1 import v1
+
+
+    from app.api.v1 import v1 as v1
     app.register_blueprint(v1)
 
     return app
--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,8 +1,7 @@
 from flask import Blueprint
-from flask_restful import Resource, Api
 
 
 from instance.config import app_config
 
 
-v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
+v1 = Blueprint('v1', __name__)

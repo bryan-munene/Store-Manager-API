d8b42482a52c96e1bc9cdc55e5239fc18cd61dbd
bryan-munene <bryanmuthuri38@gmail.com>
2018-10-26 11:18:38 +0300

    [Chore #161429228] add jwt creation at login

--- app/api/v1/views/auth.py
+++ app/api/v1/views/auth.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response, session
+from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity)
+from datetime import datetime
+
 from ..models.auth import UserModel
 from app.api.v1.utility.validators import json_checker, blanks_login, blanks_reistration, email_checker
 
@@ -9,28 +12,8 @@ users_bp = Blueprint('users', __name__, url_prefix='/api/v1')
 user_model = UserModel()
 
 class Users(object):
-    @users_bp.route("/login")
-    def home():
-        if session.get('logged_in'):
-            return make_response(jsonify({
-                "status": "user logged in",
-                "username": session['username'],
-                "login": True
-            }), 200)
-        elif session.get('logged_in_admin'):
-            return make_response(jsonify({
-                "status": "admin logged in",
-                "username": session['username'],
-                "login": True
-            }), 200)
-        else:
-            return make_response(jsonify({
-                "status": "login error",
-                "login": False
-            }), 401)
-
     @users_bp.route("/login", methods=["POST"])
-    def do_user_login():
+    def user_login():
         ok = json_checker(request)
         if not ok:
             return make_response(jsonify({
@@ -59,7 +42,7 @@ class Users(object):
         user = user_model.get_user_by_email(email)
         if user:
             user_password = user.get('password')
-            user_role = user.get('is_admin')
+            user_role =user.get('is_admin')
         else:
             return make_response(jsonify({
                 "status": "not found",
@@ -67,19 +50,30 @@ class Users(object):
             }), 404)
 
         credentials = user_model.check_password(user_password, password)
+        '''exp = datetime.timedelta(minutes=15)
+'''
         if credentials and not user_role:
-            session['username'] = user.get('username')
-            session['logged_in'] = True
-            return Users.home()
-        elif credentials and user_role:
-            session['username'] = user.get('username')
-            session['logged_in_admin'] = True
-            return Users.home()
-        else:
-            session['logged_in'] = False
-            session['logged_in_admin'] = False
+            token = create_access_token(user['email'])
+            return make_response(jsonify({
+                "status": "user logged in",
+                "username": user['username'],
+                "login": True,
+                "token": token
+                }), 200)
 
-        return Users.home()
+        elif credentials and user_role:
+            token = create_access_token(user['email'])
+            return make_response(jsonify({
+                "status": "Admin logged in",
+                "username": user['username'],
+                "login": True,
+                "token": token
+                }), 200)
+        else:
+            return make_response(jsonify({
+                "status": "login error",
+                "login": False
+                }), 401)
 
     @users_bp.route("/register", methods=["POST"])
     def register():
